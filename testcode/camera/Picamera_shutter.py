73d216197d9ec54debb8f4a96d43dce67a7b60ba
Ripe-Tomato5 <139101636+Ripe-Tomato5@users.noreply.github.com>
2025-07-17 15:50:58 +0900

    色認識のためのコード

---
+++ testcode/camera/Picamera_shutter.py
@@ -0,0 +1,22 @@
+from picamra2 import Picamera2
+import cv2
+import time
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_still_configuration())
+picam2.start()
+
+try:
+    while True:
+        picam2.capture_file("cone_1.jpg") # 画像を撮影後cone_1.jpgという名前で保存
+        print("画像を保存しました")
+        img = cv2.imread("cone_1.jpg") # 撮影した画像を読み込む
+        cv2.imshow("Captured Image", img) # 画像を表示
+        cv2.waitKey(1)
+        time.sleep(5) # 5秒待機
+
+except KeyboardInterrupt: #ctrl+cで安全に終了
+    print("撮影を終了します")
+    picam2.stop()
+    cv2.destroyAllWindows() # ウィンドウを閉じる
+    print("終了しました")
