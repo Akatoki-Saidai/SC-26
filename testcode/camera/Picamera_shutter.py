327da92a66417df9b2a16deae1232e0b02ecc126
Ripe-Tomato5 <139101636+Ripe-Tomato5@users.noreply.github.com>
2025-07-17 18:23:55 +0900

    Picamera
    画素数を指定して下げるコードを追加
    細かい間違いを修正

--- testcode/camera/Picamera_shutter.py
+++ testcode/camera/Picamera_shutter.py
@@ -1,9 +1,13 @@
-from picamra2 import Picamera2
+from picamera2 import Picamera2
 import cv2
 import time
 
 picam2 = Picamera2()
 picam2.configure(picam2.create_still_configuration())
+picam2.start()
+conf = Picamera2.create_preview_configuration(main = {"size": (640, 480)}) # プレビューの設定
+picam2.configure(conf)
+picam2.start_preview(True)
 picam2.start()
 
 try:
@@ -12,8 +16,8 @@ try:
         print("画像を保存しました")
         img = cv2.imread("cone_1.jpg") # 撮影した画像を読み込む
         cv2.imshow("Captured Image", img) # 画像を表示
-        cv2.waitKey(1)
-        time.sleep(5) # 5秒待機
+        cv2.waitKey(0)
+        time.sleep(1) # 5秒待機
 
 except KeyboardInterrupt: #ctrl+cで安全に終了
     print("撮影を終了します")
