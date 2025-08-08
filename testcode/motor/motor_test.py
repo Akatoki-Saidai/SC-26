d8e1727d666b686b5fbd5fc698208b9a0cbcdd99
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-08-08 16:39:10 +0900

    モーターテストをinputとループに

--- testcode/motor/motor_test.py
+++ testcode/motor/motor_test.py
@@ -15,14 +15,15 @@ pi.hardware_PWM(IN1_GPIO, 0, 0)
 while True:
     # Forward 20% 
     # IN2 - HI75% : LO25%
+    input("move?")
     pi.hardware_PWM(IN2_GPIO, 200000, 1000000)
     # IN1 - HI100% : LO0%
     pi.hardware_PWM(IN1_GPIO, 200000, 1000000)
-    time.sleep(60)
+    time.sleep(4)
 
     # Stop
     pi.hardware_PWM(IN2_GPIO, 0, 0)
     pi.hardware_PWM(IN1_GPIO, 0, 0)
-    time.sleep(10)
+    time.sleep(0.5)
 
 pi.stop()
