dbf25ba3c4448b0efa471938c7812e072aff3cc4
n17y17 <N17Y17@outlook.com>
2025-08-08 23:13:23 +0900

    syuusei2

--- testcode/motor/motor_test.py
+++ testcode/motor/motor_test.py
@@ -2,28 +2,32 @@
 import pigpio
 import time
 
-IN1_GPIO = 18
-IN2_GPIO = 19
+R1_GPIO = 18
+R2_GPIO = 12
+L1_GPIO = 13
+L2_GPIO = 19
 
 pi = pigpio.pi()
 
 # Stop
-pi.hardware_PWM(IN2_GPIO, 0, 0)
-pi.hardware_PWM(IN1_GPIO, 0, 0)
+pi.hardware_PWM(R1_GPIO, 0, 0)
+pi.hardware_PWM(R2_GPIO, 0, 0)
+pi.hardware_PWM(L1_GPIO, 0, 0)
+pi.hardware_PWM(L2_GPIO, 0, 0)
 # time.sleep(30)
 
 while True:
     # Forward 20% 
     # IN2 - HI75% : LO25%
     input("move?")
-    pi.hardware_PWM(IN2_GPIO, 200000, 1000000)
+    pi.hardware_PWM(L2_GPIO, 200000, 1000000)
     # IN1 - HI100% : LO0%
-    pi.hardware_PWM(IN1_GPIO, 200000, 1000000)
+    pi.hardware_PWM(R1_GPIO, 200000, 1000000)
     time.sleep(4)
 
     # Stop
-    pi.hardware_PWM(IN2_GPIO, 0, 0)
-    pi.hardware_PWM(IN1_GPIO, 0, 0)
+    pi.hardware_PWM(L2_GPIO, 0, 0)
+    pi.hardware_PWM(R1_GPIO, 0, 0)
     time.sleep(0.5)
 
 pi.stop()
