2b41b308ccfdffbf658b5bab5f481e81c347c094
n17y17 <N17Y17@outlook.com>
2025-08-10 15:52:34 +0900

    fmにmotorの偏光を移す

--- fm/motor_driver.py
+++ fm/motor_driver.py
@@ -1,49 +1,39 @@
-# このファイルは一時的にsc26のtestcode内のファイルをコピーしたものです
-
 #引用元(https://www.radical-dreamer.com/programming/raspberry-pi-pwm-motordriver/)コミット
 import pigpio
 import time
 
-IN1_GPIO = 18
-IN2_GPIO = 19
+R1_GPIO = 18
+R2_GPIO = 12
+L1_GPIO = 13
+L2_GPIO = 19
 
 pi = pigpio.pi()
 
-# Forward 20% 
-# IN2 - HI75% : LO25%
-pi.hardware_PWM(IN2_GPIO, 200000, 750000)
-# IN1 - HI100% : LO0%
-pi.hardware_PWM(IN1_GPIO, 200000, 1000000)
-time.sleep(5)
-
-# Forward 50%
-# IN2 - HI50% : LO50%
-pi.hardware_PWM(IN2_GPIO, 200000, 500000)
-# IN1 - HI100% : LO0%
-pi.hardware_PWM(IN1_GPIO, 200000, 1000000)
-time.sleep(5)
-
 # Stop
-pi.hardware_PWM(IN2_GPIO, 0, 0)
-pi.hardware_PWM(IN1_GPIO, 0, 0)
-time.sleep(2)
+pi.hardware_PWM(R1_GPIO, 200000, 0)
+pi.hardware_PWM(R2_GPIO, 200000, 0)
+pi.hardware_PWM(L1_GPIO, 200000, 0)
+pi.hardware_PWM(L2_GPIO, 200000, 0)
+# time.sleep(30)
 
-# Reverse 20%
-# IN1 - HI75% : LO25%
-pi.hardware_PWM(IN1_GPIO, 200000, 750000)
-# IN2 - HI100% : LO0%
-pi.hardware_PWM(IN2_GPIO, 200000, 1000000)
-time.sleep(5)
+try:
+    while True:
+        # Forward 20% 
+        # IN2 - HI75% : LO25%
+        input("move?")
+        pi.hardware_PWM(L2_GPIO, 200000, 1000000)
+        # IN1 - HI100% : LO0%
+        pi.hardware_PWM(R1_GPIO, 200000, 1000000)
+        time.sleep(4)
 
-# Reverse 50%
-# IN1 - HI50% : LO50%
-pi.hardware_PWM(IN1_GPIO, 200000, 500000)
-# IN2 - HI100% : LO0%
-pi.hardware_PWM(IN2_GPIO, 200000, 1000000)
-time.sleep(5)
-
-# Stop
-pi.hardware_PWM(IN1_GPIO, 0, 0)
-pi.hardware_PWM(IN2_GPIO, 0, 0)
-
-pi.stop()
+        # Stop
+        pi.hardware_PWM(L2_GPIO, 200000, 0)
+        pi.hardware_PWM(R1_GPIO, 200000, 0)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print("終了します。PWMを停止します。")
+    pi.hardware_PWM(L2_GPIO, 0, 0)
+    pi.hardware_PWM(R1_GPIO, 0, 0)
+    pi.hardware_PWM(R2_GPIO, 0, 0)
+    pi.hardware_PWM(L1_GPIO, 0, 0)
+    pi.stop()
