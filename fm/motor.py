c2afff65decc1c289e772698a95cc5e234f1a6c6
n17y17 <N17Y17@outlook.com>
2025-08-10 16:22:56 +0900

    motorを更新

--- fm/motor.py
+++ fm/motor.py
@@ -1,39 +1,96 @@
 #引用元(https://www.radical-dreamer.com/programming/raspberry-pi-pwm-motordriver/)コミット
+from logging import getLogger, StreamHandler
 import pigpio
 import time
 
-R1_GPIO = 18
-R2_GPIO = 12
-L1_GPIO = 13
-L2_GPIO = 19
+class Motor:
+    def __init__(self, right_pin1=18, right_pin2=12, left_pin1=13, left_pin2=19, logger=None):
+        if logger is None:
+            logger = getLogger(__name__)
+            logger.addHandler(StreamHandler())
+            logger.setLevel(10)  # DEBUGレベル
+        self._logger = logger
 
-pi = pigpio.pi()
-
-# Stop
-pi.hardware_PWM(R1_GPIO, 200000, 0)
-pi.hardware_PWM(R2_GPIO, 200000, 0)
-pi.hardware_PWM(L1_GPIO, 200000, 0)
-pi.hardware_PWM(L2_GPIO, 200000, 0)
-# time.sleep(30)
-
-try:
-    while True:
-        # Forward 20% 
-        # IN2 - HI75% : LO25%
-        input("move?")
-        pi.hardware_PWM(L2_GPIO, 200000, 1000000)
-        # IN1 - HI100% : LO0%
-        pi.hardware_PWM(R1_GPIO, 200000, 1000000)
-        time.sleep(4)
+        self._right_pin1 = right_pin1
+        self._right_pin2 = right_pin2
+        self._left_pin1 = left_pin1
+        self._left_pin2 = left_pin2
+        
+        self._pi = pigpio.pi()
 
         # Stop
-        pi.hardware_PWM(L2_GPIO, 200000, 0)
-        pi.hardware_PWM(R1_GPIO, 200000, 0)
-        time.sleep(0.5)
-except KeyboardInterrupt:
-    print("終了します。PWMを停止します。")
-    pi.hardware_PWM(L2_GPIO, 0, 0)
-    pi.hardware_PWM(R1_GPIO, 0, 0)
-    pi.hardware_PWM(R2_GPIO, 0, 0)
-    pi.hardware_PWM(L1_GPIO, 0, 0)
-    pi.stop()
+        self._pi.hardware_PWM(self._right_pin1, 200000, 0)
+        self._pi.hardware_PWM(self._right_pin2, 200000, 0)
+        self._pi.hardware_PWM(self._left_pin1, 200000, 0)
+        self._pi.hardware_PWM(self._left_pin2, 200000, 0)
+    
+    def set_right(self, speed):
+        """右モーターの速度を設定"""
+        if speed > 1:
+            speed = 1000000
+        else:
+            speed = int(speed * 1000000)
+
+        if speed >= 0:
+            self._pi.hardware_PWM(self._right_pin1, 200000, speed)
+            self._pi.hardware_PWM(self._right_pin2, 200000, 0)
+        elif speed < 0:
+            self._pi.hardware_PWM(self._right_pin1, 200000, 0)
+            self._pi.hardware_PWM(self._right_pin2, 200000, -speed)
+    
+    def set_left(self, speed):
+        """左モーターの速度を設定"""
+        if speed > 1:
+            speed = 1000000
+        else:
+            speed = int(speed * 1000000)
+
+        if speed >= 0:
+            self._pi.hardware_PWM(self._left_pin1, speed, 200000)
+            self._pi.hardware_PWM(self._left_pin2, 0, 200000)
+        elif speed < 0:
+            self._pi.hardware_PWM(self._left_pin1, 0, 200000)
+            self._pi.hardware_PWM(self._left_pin2, -speed, 200000)
+    
+    def stop(self):
+        """モーターを停止"""
+        self._pi.hardware_PWM(self._right_pin1, 200000, 0)
+        self._pi.hardware_PWM(self._right_pin2, 200000, 0)
+        self._pi.hardware_PWM(self._left_pin1, 200000, 0)
+        self._pi.hardware_PWM(self._left_pin2, 200000, 0)
+
+    def turn(self, angle):
+        """指定した角度だけ回転
+        angle: 0~360度，0:前進，90:右旋回，...
+        """
+        if 0 <= angle and angle < 180:
+            self.set_right(1 - angle / 90)
+            self.set_left(1)
+        elif 180 <= angle and angle <= 360:
+            self.set_right(1)
+            self.set_left(1 - (angle - 180) / 90)
+        else:
+            self.set_right(0)
+            self.set_left(0)
+    
+    def __del__(self):
+        """インスタンスが削除されるときにモーターを停止"""
+        self.stop()
+        self._pi.stop()
+
+
+if __name__ == "__main__":
+    motor = Motor()
+
+    try:
+        while True:
+            # Forward
+            motor.turn(0)
+            time.sleep(2)
+
+            # Stop
+            motor.stop()
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("終了します。PWMを停止します。")
+        motor.stop()
