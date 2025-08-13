f0e91fc593d09071ebfd649c67e12e8c6b6c4115
n17y17 <N17Y17@outlook.com>
2025-08-14 01:34:16 +0900

    nicr 番号と雲お修正

--- testcode/nicr/nicr.py
+++ testcode/nicr/nicr.py
@@ -2,7 +2,7 @@ import pigpio
 import time
 
 if __name__ == "__main__":
-    NiCr_GPIO = 9
+    NiCr_GPIO = 6
 
     pi = pigpio.pi()
 
@@ -14,8 +14,8 @@ if __name__ == "__main__":
         input("NiCrをONにしますか？ (y/n): ")
         user_input = input().strip().lower()
         if user_input == 'y':
+            print("NiCrをONにします。")
             pi.write(NiCr_GPIO, 1)  # NiCrをONにする
-            print("NiCrをONにしました。")
             time.sleep(5)  # 5秒間ONにする
             pi.write(NiCr_GPIO, 0)  # NiCrをOFFにする
             print("NiCrをOFFにしました。")
